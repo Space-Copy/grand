2fc1995d392049d3f566e1b194e173f299e23964
Rikazavr <risempai@gmail.com>
2016-04-15 13:24:45 +0000

    PIPETS

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,20 @@
 // This is where it all goes :)
+
+jQuery(document).ready(function($){
+
+    //открытие модального окна
+	$('.open_modal').click(function (e){
+		e.preventDefault();
+		$('.popup, .overlay').css({'opacity':'1', 'display':'block'});
+	});
+    
+    //закрытие модального окна
+	$('.close_modal, .overlay').click(function (){
+		$('.popup, .popup2, .overlay').css({'opacity':'0', 'display':'none'});
+		$('.popup > .fofm textarea').val('');
+		//сброс всех полей формы обраной связи
+		$(':input','.fofm').not(':button, :submit, :reset, :hidden').val('').removeAttr('checked').removeAttr('selected');
+		$(".fofm input[type=submit]").attr('disabled','disabled');
+	});
+
+});
