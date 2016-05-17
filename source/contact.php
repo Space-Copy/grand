8eaca10a6337f39a97ade8d73b2699025f1e5b79
Rikazavr <risempai@gmail.com>
2016-05-17 18:43:54 +0000

    last Mavasha version

---
+++ source/contact.php
@@ -0,0 +1,42 @@
+<?php
+//проверяем значения полученые при проверке скриптом формы
+if (trim($_POST['valTrFal'])!='valTrFal_true') {
+	echo 'fasle';
+}
+else {
+
+		$childname = trim($_POST['childname']);
+
+		$birthday = trim($_POST['birthday']);
+		
+		$parentname = trim($_POST['parentname']);
+
+		$phone = trim($_POST['phone']);
+		
+		$rank = trim($_POST['rank']);
+		
+		$sport = trim($_POST['sport']);
+		
+		$additional = trim($_POST['additional']);
+
+
+		// от кого
+		$fromMail = 'info@sc-grand.ru';
+		$fromName = 'sc-grand';
+
+		// Сюда введите Ваш email
+		$emailTo = 'risempai@gmail.com';
+
+		$subject = 'Форма обратной связи';
+		$subject = '=?utf-8?b?'. base64_encode($subject) .'?=';
+		$headers = "Content-type: text/plain; charset=\"utf-8\"\r\n";
+		$headers .= "From: ". $fromName ." <". $fromMail ."> \r\n";
+
+		// тело письма
+		$body = "Получено заявка с сайта sc-grand\n\nИмя ребёнка: $childname\nДата рождения: $birthday\nФИО родителя: $parentname\nТелефон: $phone\nСпортивный разряд: $rank\nВид спорта: $sport\nСообщение: $additional";
+		$mail = mail($emailTo, $subject, $body, $headers, '-f'. $fromMail );
+
+
+		echo 'ok';
+}
+?>
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,41 +1,50 @@
 // This is where it all goes :)
 
 jQuery(document).ready(function($){
-
+	
     //открытие модального окна
-	$('.open_modal').click(function (e){
+	$('.open_modal, .registration-button, .standart-page-reg-button, .corner-registration-button').click(function (e){
 		e.preventDefault();
-		$('.popup, .overlay').css({'opacity':'1', 'display':'block'});
+		$('.popup, .overlay, .form-wrapper').css({'opacity':'1', 'display':'block'});
 	});
-    
+	
     //закрытие модального окна
 	$('.close_modal, .overlay').click(function (){
-		$('.popup, .popup2, .overlay').css({'opacity':'0', 'display':'none'});
-		$('.popup > .fofm textarea').val('');
+		console.log("sup");
+		$('.popup, .popup2, .overlay, .form-wrapper').css({'opacity':'0', 'display':'none'});
+		$('.popup > .reg-form textarea').val('');
+		
 		//сброс всех полей формы обраной связи
-		$(':input','.fofm').not(':button, :submit, :reset, :hidden').val('').removeAttr('checked').removeAttr('selected');
-		$(".fofm input[type=submit]").attr('disabled','disabled');
+		$(':input','.reg-form').not(':button, :submit, :reset, :hidden').val('').removeAttr('checked').removeAttr('selected');
+		$(".reg-form input[type=submit]").attr('disabled','disabled');
+	});
+	
+	//аякс форма обратной связи
+	//проверяет какой ответ был получен
+	//и в зависимости от ответа
+	//выводит информацию о статусе отправки письма
+	$(".reg-form").submit(function() {
+		var str = $(this).serialize();
+		$.ajax({
+			type: "POST",
+			url: "contact.php",
+			data: str,
+			success: function(msg) {
+				if(msg == 'ok') {
+					$('.popup2, .overlay').css('opacity','1');
+					$('.popup2, .overlay').css('visibility','visible');
+					$('.popup').css({'opacity':'0','visibility':'hidden'});
+				}
+				else {
+					$('.popup2 .window').html('<h5>Ошибка</h5><p>Сообщение не отправлено, убедитесь в правильности заполнение полей</p>');
+					$('.popup2, .overlay').css('opacity','1');
+					$('.popup2, .overlay').css('visibility','visible');
+					$('.popup').css({'opacity':'0','visibility':'hidden'});
+				}
+			}
+		});
+		return false;
 	});
 
 });
-
-// аякс срань
-
-$(function() {
-  
-  $('#contactPageForm form').on('submit', function(e){
-    e.preventDefault();
-    
-    var data = $(this).serialize(),
-        url = $(this).attr('action');
-    
-    $.post(url, data, function(data){
-      alert('Спасибо, ваша заявка принята!');
-    });
-    
-  });
-  
-});
-
-//что же я творю то
 
